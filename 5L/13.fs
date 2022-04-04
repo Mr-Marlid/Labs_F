be6e97986888f83e91e8c073d5a72ade6399780c
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2022-04-04 20:42:15 +0300

    Add files via upload

---
+++ 5L/13.fs
@@ -0,0 +1,20 @@
+﻿open System
+
+let rec F2 a s =
+    match a with
+    |0 -> s
+    |a -> F2 (a/10) ((a%10)*s)
+
+let rec F1 a =
+    if (a <> 0) then (a%10) * F1 (a/10)
+    else 1
+        
+[<EntryPoint>]
+let main argv =
+    printfn "Введите число:"
+    let a = int(Console.ReadLine())
+    printfn "Up:"
+    Console.WriteLine(F1 a)
+    printfn "Down:"
+    Console.WriteLine(F2 a 1)
+    0
