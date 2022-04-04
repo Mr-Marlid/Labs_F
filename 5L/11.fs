b42e79f799ef7d69db3ee9d433abc6c94038588b
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2022-04-04 20:41:35 +0300

    Add files via upload

---
+++ 5L/11.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let f s = 
+    match s with
+        |"F#" | "Prolog" -> printfn "Подлиза"
+        |"python" -> printfn "Ну, можешь забыть его до 3 курса"
+        |"С++" -> printfn "А, это что-то типо паскаля?"
+        |"ruby" -> printfn "Не смей."
+        |"С#" -> printfn "Садо-мазо?"
+        |other -> printfn "Мы таких не понимаем"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Введите свой любимый язык программирования:"
+    let s = Console.ReadLine()
+    f s
+    0
