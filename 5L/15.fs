55a98d71ecf44990291ea71d6d7e446692418dde
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2022-04-04 22:45:39 +0300

    Add files via upload

---
+++ 5L/15.fs
@@ -0,0 +1,24 @@
+﻿open System
+let rec prost x y z= 
+    if y <= z then 1
+    else if x%z=0 && y%z=0 then 0
+    else prost x y z+1
+
+let F x func s =
+    let rec f x func s a =
+        if a <> 0 then 
+            let s1= if prost x a 2 = 1 then func s a else s
+            let a1 = a - 1
+            f x func s1 a1
+        else
+            s
+    f x func s x
+        
+[<EntryPoint>]
+let main argv =
+    Console.WriteLine("Number: ")
+    let x = int(Console.ReadLine())
+    let result = F x (fun x y -> x+y) 0
+    Console.Write("Result: ")
+    Console.WriteLine result
+    0
