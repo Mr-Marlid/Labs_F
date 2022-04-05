6687ac76495b463eaef646babb5b5f9a9aedc8ed
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2022-04-05 13:20:45 +0300

    Update 15.fs

--- 5L/15.fs
+++ 5L/15.fs
@@ -1,24 +1,27 @@
-﻿open System
-let rec prost x y z= 
-    if y <= z then 1
-    else if x%z=0 && y%z=0 then 0
-    else prost x y z+1
+open System
 
-let F x func s =
-    let rec f x func s a =
+let rec prost x y z=
+    let z1 = z-1
+    if z=1 then 1 
+    else if x%z=0 && y%z=0 then 0
+    else prost x y z1
+
+let rec f x func s a =
         if a <> 0 then 
-            let s1= if prost x a 2 = 1 then func s a else s
+            let s1= if prost x a a = 1 then func s a else s
             let a1 = a - 1
             f x func s1 a1
         else
             s
+
+let F x func s =
     f x func s x
         
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine("Number: ")
+    Console.WriteLine("Введите число: ")
     let x = int(Console.ReadLine())
-    let result = F x (fun x y -> x+y) 0
-    Console.Write("Result: ")
-    Console.WriteLine result
+    let res = F x (fun x y -> x+y) 0
+    Console.Write("Результат: ")
+    Console.WriteLine res
     0
