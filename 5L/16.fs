5df2450b1b6445855facb1af38e1cd0a887bc65d
Mr-Marlid <78685037+Mr-Marlid@users.noreply.github.com>
2022-05-17 20:25:30 +0300

    Update 16.fs

--- 5L/16.fs
+++ 5L/16.fs
@@ -1,47 +1,45 @@
 open System
-
-let rec NOD x y z=
+let rec prost x y z=
     let z1 = z-1
     if z=1 then 1 
     else if x%z=0 && y%z=0 then z
-    else NOD x y z1
-
+    else prost x y z1
 let rec f x func s a =
         if a <> 0 then 
-            let s1= if NOD x a a <> 1 then func s a else s
+            let s1= if prost x a a = 1 then func s a else s
             let a1 = a - 1
             f x func s1 a1
         else
             s
-
-
-let rec e x func s a = 
-        if x <> 1 then 
-            if x%a=0 then 
-                let x1 = x/a
-                let s1 = func s a-1
-                e x1 func s1 a
-            else
-                let a1 = a + 1
-                e x func s a1
-        else
-            s
-
-let  E x func s =
-    e x func s 2
-
 let F x func s =
-    f x func s x
-        
+    f x func s x-1
+let rec E x y n =
+    let y1 = y - 1
+    if (y>1) then
+        if (prost x y y = 1) then
+            
+            let n1 = n + 1
+            E x y1 n1
+        else E x y1 n
+    else n
 [<EntryPoint>]
 let main argv =
-    Console.WriteLine("Введите число: ")
+    printfn "%A" ("Введите число:")
     let x = int(Console.ReadLine())
-    let res = F x (fun x y -> x+y) 0
-    Console.Write("Результат NOD: ")
-    Console.WriteLine res
-
-    let res1 = E x (fun x y -> x*y) 1
-    Console.Write("Результат E: ")
-    Console.WriteLine res1
+    printfn "%A""Введите функцию: "
+    let s = string(Console.ReadLine())
+    printfn "%A" "Обход взаимно простых компонентов числа"
+    match s with
+    |"Сумма" |"сумма" -> printfn "%A" (F x (fun x y -> x+y) 0)
+    |"Произведение"|"Произведение" -> printfn "%A" (F x (fun x y -> x+y) 0)
+    |"Минимум"|"минимум" -> printfn "%A" (F x (fun x y -> if (x<y) then x else y) 0) 
+    |"Максимум"|"максимум" -> printfn "%A" (F x (fun x y -> if (x>y) then x else y) 0)
+    printfn "%A" "Число Эйлера " 
+    let x1 = x-1
+    printfn "%A" (E x x1 0)
+    printfn "%A" "Введите второе число для НОД " 
+    let y = int(Console.ReadLine())
+    let y1 = y-1
+    printfn "%A" "НОД "
+    printfn "%A" (prost x y y1)
     0
